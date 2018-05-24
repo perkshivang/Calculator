b7d3844c54c9efab2bfe66a69e16d9d60d7fc626
shivang <shivang@perk.com>
2018-05-24 14:21:34 +0530

    adding xcode test app.

---
+++ CalculatorSample/CalculatorSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CalculatorSample
+//
+//  Created by shivang on 5/24/18.
+//  Copyright © 2018 shivang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CalculatorSample/CalculatorSample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  CalculatorSample
+//
+//  Created by shivang on 5/24/18.
+//  Copyright © 2018 shivang. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
