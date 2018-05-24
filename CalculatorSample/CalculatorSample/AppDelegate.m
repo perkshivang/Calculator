56d9c01fea0f795911af9b877b8e73e03edffaa1
shivang <shivang@perk.com>
2018-05-24 20:44:28 +0530

    test comment added.

--- CalculatorSample/CalculatorSample/AppDelegate.m
+++ CalculatorSample/CalculatorSample/AppDelegate.m
@@ -16,6 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    //test comment
     // Override point for customization after application launch.
     return YES;
 }
